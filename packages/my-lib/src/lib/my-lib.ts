4973590f19d069d903015a3ccd2c7f3b4992712a
Thomas Dekiere <t.dekiere@gmail.com>
2024-02-17 23:05:37 +0100

    feat(my-lib): add a special feature 2

--- packages/my-lib/src/lib/my-lib.ts
+++ packages/my-lib/src/lib/my-lib.ts
@@ -5,3 +5,7 @@ export function myLib(): string {
 export function myNewFeature(): string {
   return 'my-new-feature';
 }
+
+export function myNewFeature2(): string {
+  return 'my-new-feature-2';
+}
